6a48782a0b3a480f113ac3e4b8902e5dcbbf7feb
Christopher Reeve <cjreeve@gmail.com>
2013-02-13 14:16:13 +0000

    test to get sample_app uploading

---
+++ ruby-railstutorial-org/app3_sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'abb116de98905db112bf18e161a56d6bc390e05e092d489ced69b5a8ba45e2e5994669476b0749aaf03db0481e95c2a5e945a8a3fc7edf0e759462494bacc1e6'
